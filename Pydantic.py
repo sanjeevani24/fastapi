ddc5ddd918459e4bf7a8f9a38945d3c7c4c6012e
sanjeevani24 <sanjeevanism2020@gmail.com>
2026-06-16 15:43:30 +0530

    Added field Validator

--- Pydantic.py
+++ Pydantic.py
@@ -1,25 +1,44 @@
-from pydantic import BaseModel
+from pydantic import BaseModel , EmailStr , AnyUrl , Field
+from typing import List, Dict , Optional, Annotated
 
+#TYPE VALIDATION USING PYDANTIC field function is used to add additional validation rules to the field.  gt means greater than, lt means less than, strict means that the value must be of the specified type and cannot be coerced to that type.  default means that the field is not required and can be None.  description is used to add a description to the field in the docs.
 class Patient(BaseModel):
 
-    name: str
-    age: int 
-    weight: float     
+    name: Annotated[str, Field(max_length = 50, title='Name of the Patient')]  #CAN ADDDESCRIPTION, TITLE, EXAMPLE, MAX_LENGTH, MIN_LENGTH, REGEX ETC.  TO THE FIELD USING FIELD FUNCTION.  ANNOTATED IS USED TO ADD ADDITIONAL METADATA TO THE FIELD.
+    age: int = Field(gt=0, lt=120)
+    email: EmailStr         #Data validation for email using pydantic
+    website: Optional[AnyUrl] = None        #Data validation for URL using pydantic.  Optional means that the field is not required and can be None.
+    weight: Annotated[float, Field(gt=0, strict=True)]             #Data validation for weight using pydantic.  gt means greater than 0.  Field is used to add additional validation rules to the field.
+    married: Annotated[bool, Field(default=None, description='Marital status of the patient')]  #Data validation for boolean using pydantic.  default means that the field is not required and can be None.
+    allergies: Annotated[Optional[List[str]], Field(max_length = 5, default=None)]
+    contact_details: Dict[str, str]
     
 def insert_patient_data(patient: Patient):
 
     print(patient.name)
-    print(patient.age)
+    print(patient.age) 
     print("Inserted")
 
 def update_patient_data(patient: Patient):
 
     print(patient.name)
     print(patient.age)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print(patient.website)
+    print(patient.email)
     print("Updated")
 
-patient_info = {'name': 'Sanjeevani', 'age': 20}
+patient_info = {'name': 'Sanjeevani', 'age': 20, 'email': 'sm2020@gmail.com', 'weight': 55.5, 'married': False, 'contact_details': {'email': 'sm2020@gmail.com', 'phone': '1234567890'}}
 
 patient1 = Patient(**patient_info)
 
-insert_patient_data(patient1)
+#insert_patient_data(patient1)
+update_patient_data(patient1)
+
+#DATA VALIDATION USING PYDANTIC
+
+
+
---
+++ field_validator.py
@@ -0,0 +1,59 @@
+from pydantic import BaseModel , EmailStr , AnyUrl , Field, field_validator
+from typing import List, Dict , Optional, Annotated
+
+#DATA VALIDATION USING PYDANTIC
+
+class Patient(BaseModel):
+
+    name: str
+    age: int 
+    email: EmailStr        
+    weight: float            
+    married: bool 
+    allergies: List[str]
+    contact_details: Dict[str, str]
+
+    @field_validator('email')
+    @classmethod
+    def email_validator(cls, value):
+
+        valid_domains = ['hdfc.com', 'icici.com']
+
+        domain_name = value.split('@')[-1]
+
+        if domain_name not in valid_domains:
+            raise ValueError('Not a valid email domain.')
+         
+        return value
+    
+    @field_validator('name')
+    @classmethod
+    def transform_name(cls,value):
+        return value.upper()
+
+
+
+def insert_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age) 
+    print("Inserted")
+
+def update_patient_data(patient: Patient):
+
+    print(patient.name)
+    print(patient.age)
+    print(patient.email)
+    print(patient.weight)
+    print(patient.married)
+    print(patient.allergies)
+    print(patient.contact_details)
+    print("Updated")
+
+patient_info = {'name': 'Sanjeevani', 'age': 20, 'email': 'sm2020@icici.com', 'weight': 55.5, 'married': False, 'allergies': ['Peanuts'], 'contact_details': {'email': 'sm2020@gmail.com', 'phone': '1234567890'}}
+
+patient1 = Patient(**patient_info)
+
+#insert_patient_data(patient1)
+update_patient_data(patient1)
+
